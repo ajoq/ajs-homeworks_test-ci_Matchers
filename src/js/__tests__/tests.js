bf0b93135f744dd2f21dc0492bc9655d025c074e
Konstantin Beloglazov <ajoq@ya.ru>
2021-10-25 11:54:28 +0300

    done

--- src/js/__tests__/tests.js
+++ src/js/__tests__/tests.js
@@ -1,15 +1,15 @@
 import sortHeroes from '../app';
 
 test('sort by health', () => {
-    const heroes = [
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-      ];
-    const heroesHealth = [
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-      ];
-      expect(sortHeroes(heroes)).toEqual(heroesHealth);
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const heroesHealth = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(sortHeroes(heroes)).toEqual(heroesHealth);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 export default function sortHeroes(arr) {
-    return arr.sort((a, b) => b.health - a.health);
-  };
+  return arr.sort((a, b) => b.health - a.health);
+}
