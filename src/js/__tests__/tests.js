ab4df1aab3df3261a88e35569f7168fdc294c5ab
Konstantin Beloglazov <ajoq@ya.ru>
2021-10-27 10:29:41 +0300

    done v2

--- src/js/__tests__/tests.js
+++ src/js/__tests__/tests.js
@@ -1,5 +1,20 @@
 import sortHeroes from '../app';
 
+test('sort by health toBe', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const heroesHealth = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = sortHeroes(heroes)
+  expect(Object.is(result, heroesHealth)).toBe(false)
+});
+
 test('sort by health', () => {
   const heroes = [
     { name: 'мечник', health: 10 },
